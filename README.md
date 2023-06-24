4f0efffc4d4c4927fbd2512996db29ae474353a3
njit_77 <512424964@qq.com>
2023-06-24 14:58:47 +0800

    1、更新PropertyGroup设置信息，用于控制包属性

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # WPFZeroVersion
 WPF框架开发上位机软件的起始版本。
+
+
+
+##### 开发工具	Visual Studio 2022 Community
+
+##### 目标框架	.NET 6.0
+
---
+++ WPFZeroVersion/AssemblyGitInfo.cs
@@ -0,0 +1,5 @@
+/// Auto generate file version 
+using System.Reflection;  
+[assembly: AssemblyVersion("1.0.2023.2")]  
+[assembly: AssemblyFileVersion("1.0.2023.2")]  
+[assembly: AssemblyTitle("551de9c   2023.06.24 / 14:54:48.79")]  
