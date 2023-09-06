66349671441ac213d5895a85ad318a9e355e8dd4
njit_77 <512424964@qq.com>
2023-09-06 19:42:56 +0800

    feat:微调配置

--- WPF.Zero/Properties/AssemblyGitInfo.cs
+++ WPF.Zero/Properties/AssemblyGitInfo.cs
@@ -1,6 +1,6 @@
 /// Auto generate file version 
 using System.Reflection;  
-[assembly: AssemblyVersion("1.0.2023.7")]  
-[assembly: AssemblyFileVersion("1.0.2023.7")]  
-[assembly: AssemblyTitle("718df90   2023.08.19 /  9:41:01.98")]  
-[assembly: AssemblyCopyright("Copyright © 2020-2023 njit-77. All rights reserved.")]   
+[assembly: AssemblyVersion("1.0.2023.8")]
+[assembly: AssemblyFileVersion("1.0.2023.8")]
+[assembly: AssemblyTitle("cf0c13a   2023.09.06 / 15:54:09.83")]
+[assembly: AssemblyCopyright("Copyright © 2020-2023 njit-77. All rights reserved.")]
